91615fb4e4cfec1e8525d3c20fec9e455dfc05f9
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-19 18:30:16 -0600

    updated file path for user.service.ts

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
-import {Observable} from "rxjs/internal/Observable";
+import {Observable} from "rxjs/Observable";
 import {User} from "../interfaces/user";
 
 @Injectable()
