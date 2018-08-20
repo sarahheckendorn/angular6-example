4599c866e58217a25913346a05eb93c20abae8f8
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-19 18:35:52 -0600

    updated code for webpack.live.js

--- webpack/webpack.live.js
+++ webpack/webpack.live.js
@@ -1,14 +1,14 @@
-var webpack = require("webpack");
-var webpackMerge = require("webpack-merge");
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var commonConfig = require("./webpack.common.js");
-var helpers = require("./helpers");
+let webpack = require("webpack");
+let webpackMerge = require("webpack-merge");
+let MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let commonConfig = require("./webpack.common.js");
+let helpers = require("./helpers");
+let targetUrl = require("./target.js");
 
-const ENV = process.env.NODE_ENV = process.env.ENV = "production";
+const ENV = process.env.NODE_ENV = process.env.ENV = "live";
 
 module.exports = webpackMerge(commonConfig, {
-	devtool: "source-map",
-
+	mode: "production",
 	output: {
 		path: helpers.root("public_html/dist"),
 		publicPath: "dist",
@@ -16,18 +16,22 @@ module.exports = webpackMerge(commonConfig, {
 		chunkFilename: "[id].[hash].chunk.js"
 	},
 
+	performance: {
+		hints: false
+	},
+
 	plugins: [
 		new webpack.NoEmitOnErrorsPlugin(),
-		new webpack.optimize.UglifyJsPlugin(),
-		new ExtractTextPlugin("[name].[hash].css"),
+		new MiniCssExtractPlugin({filename: "[name].[hash].css"}),
 		new webpack.DefinePlugin({
 			"process.env": {
+				"BASE_HREF": JSON.stringify(targetUrl().substring(targetUrl().indexOf("/", targetUrl().indexOf("//") + 2))),
 				"ENV": JSON.stringify(ENV)
 			}
 		}),
 		new webpack.LoaderOptionsPlugin({
 			htmlLoader: {
-				minimize: false // workaround for ng5
+				minimize: false // workaround for ng2
 			}
 		})
 	]
