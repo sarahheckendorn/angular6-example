60d622bf0d48f31e6d8050cb0ef0783981100fb3
Sarah Heckendorn <praisinghim@jcangus.com>
2018-08-19 18:26:25 -0600

    updated code for vendor.ts

--- src/vendor.ts
+++ src/vendor.ts
@@ -12,5 +12,9 @@ import "rxjs";
 // You can import js, ts, css, sass, ...
 import "jquery";
 import "bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "font-awesome-webpack2";
+
+// font awesome
+import "@fortawesome/fontawesome";
+import "@fortawesome/fontawesome-free-brands";
+import "@fortawesome/fontawesome-free-regular";
+import "@fortawesome/fontawesome-free-solid";
